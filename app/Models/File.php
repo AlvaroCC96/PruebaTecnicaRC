c46c788470f86168d89c85e2cd35ed142146be04
Alvaro Castillo <alvarolucascc96@gmail.com>
2021-08-12 11:07:08 -0400

    feat(Files): implemented files migration and model

---
+++ app/Models/File.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'file_name',
+        'extension',
+    ];
+
+    /**
+     * Get the user that owns the file.
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
