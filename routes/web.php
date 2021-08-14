0a5a79a098fce30e40d7ee01db5930d6adbe209e
Alvaro Castillo <alvarolucascc96@gmail.com>
2021-08-14 01:36:21 -0400

    feat(Route): configure web.php and app.php

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/listAdmin',[FileController::class,'listAdmin'])->name('listAdmin')->middleware('auth');
+Route::get('/listBasic',[FileController::class,'listBasic'])->name('listBasic')->middleware('auth');
+Route::get('/uploadBasic',[FileController::class,'uploadBasic'])->name('uploadBasic')->middleware('auth');
+Route::post('/upload',[FileController::class,'fileSaveBasic'])->name('fileSaveBasic')->middleware('auth');
+Route::get('/download/{id}',[FileController::class,'download'])->name('download')->middleware('auth');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
 Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
-    Route::resource('files', FileController::class);
+    Route::resource('files', FileController::class);   
 });
+
