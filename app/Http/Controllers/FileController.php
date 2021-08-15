81d8cd10ae968b516c6b255052a645aa1b97d69d
Alvaro Castillo <alvarolucascc96@gmail.com>
2021-08-15 01:33:43 -0400

    refactor(Controller): :bug: refactored FileController and fix RegisterController

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -27,84 +27,134 @@ class FileController extends Controller
         $this->middleware('permission:file-upload', ['only' => ['create', 'store']]);
         $this->middleware('permission:file-assing', ['only' => ['edit', 'update']]);
     }
-
+   
     /**
      * Generate a view with all files on the system
+     * this method is validated with middleware aut
+     *
+     * @return view
      */
     public function listAdmin()
     {
-        try {
-            $hasPermission = Auth::user()->hasPermissionTo('file-assing');
-            if ($hasPermission) {
-                $files = File::all();
-                return view('files.list', compact('files'));
-            } else {
-                abort(404);
-            }
-
-        } catch (Exception $e) {
+        $hasPermission = Auth::user()->hasPermissionTo('file-assing');
+        if ($hasPermission) {
+            $files = File::all();
+            return view('files.list', compact('files'));
+        } else {
             abort(404);
         }
     }
 
     /**
      * Generate a view with the files of a user X
+     * this method is validated with middleware auth
+     *
+     * @return view
      */
     public function listBasic(){
-        try{
-            $id =Auth::id();
-            $files = File::where('user_id',$id)->get();
-            return view('files.list', compact('files'));
-        } catch (Exception $e) {
-            abort(505);
-        }
+        $id =Auth::id();
+        $files = File::where('user_id',$id)->get();
+        return view('files.list', compact('files'));   
     }
+
     /**
-     * Get a view for upload file
+     * get basic view for upload file
+     *
+     * @return view
      */
     public function uploadBasic(){
         return view('files.uploadBasic');
     }
 
-    public function fileSaveBasic(Request $request){
-        /** 
-         * Asumo cualquier tamaño para los archivos, de lo contrario se debe modificar
-        */
-        // $max_size = (int)ini_get('upload_max_size') * 10240;
+        
+    /**
+     * get admin view for upload file
+     *
+     * @return view
+     */
+    public function uploadAdmin(){
+        $users = User::all();
+        return view('files.uploadAdmin', compact('users'));
+    }
+        
+    /**
+     * Save files to database , generla method for admin/basic user
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function saveDataByTag(Request $request) {
+        isset($request->name)? $user_id = $request->names[0]: $user_id = Auth::id(); 
         $files = $request->file('files');
-        if ($files == null) {
-            Alert::error('Error','No hay archivos para subir');
+        if ($files != null) {
+            foreach($files as $file){
+                $this->saveFile($file,$user_id) == true? $isError = false:  $isError = true;
+                if($isError){
+                    Alert::error('Error', 'Ha ocurrido un error');
+                    return back();
+                }
+            }
+            Alert::success('Completado', 'Carga de archivos exitosa');
             return back();
         }
-        $user_id = Auth::id();
-        foreach ($files as $file) {
-                
-            $full_name = $file->getClientOriginalName();
-            $extension = pathinfo($fullName, PATHINFO_EXTENSION);
-            $file_name = pathinfo($fullName,PATHINFO_FILENAME);
-            $route = '/'.'public/'.$user_id.'/';
-
-            if(Storage::putFileAs($route,$file,$fullName)){
-                File::create([
-                    'file_name' => $file_name,
-                    'extension'=> $extension,
-                    'route' => $route.$full_name, 
-                    'user_id'=> $user_id, 
-                ]);
-            } else {
-                Alert::error('Error', 'Ha ocurrido un error');
-                return back();
-            } 
-        }
-        Alert::success('Completado', 'Archivos subidos con exito');
+        Alert::error('Error','No hay archivos para subir');
         return back();
     }
-
+        
     /**
-     * Download file by id
+     * Save file in storage and database
+     *
+     * @param  mixed $file
+     * @param  mixed $user_id
+     * @return boolean
+     */
+    public function saveFile($file,$user_id){
+        $route = '/'.'public/'.$user_id.'/';
+        $data = $this->getDataFromFile($file);
+        if(Storage::putFileAs($route,$file,$data[0])){
+            File::create([
+                'file_name' => $data[2],
+                'extension'=> $data[1],
+                'route' => $route.$data[0], 
+                'user_id'=> $user_id, 
+            ]);
+            return true;
+        } 
+        return false;
+    }
+        
+    /**
+     * Return array with path parts from file
+     *
+     * @param  mixed $file
+     * @return array
+     */
+    public function getDataFromFile($file){
+        $full_name = $file->getClientOriginalName();
+        return array($full_name,
+            pathinfo($full_name, PATHINFO_EXTENSION),
+            pathinfo($full_name,PATHINFO_FILENAME)
+        );
+    }
+        
+    /**
+     * Download a file from database and storage
+     *
+     * @param  mixed $id
+     * @return void
      */
     public function download($id){
-        $file = File::select('route')->where('id',$id)->get()[0];
-        return Storage::download($file['route']);  
+        $isAdmin= Auth::user()->hasRole('Admin');
+        try{
+            $file = File::select()->where('id',$id)->get()[0];
+            if ($isAdmin || $file['user_id'] == Auth::id()) {
+                return Storage::download($file['route']); 
+            }
+            Alert::error('Error', 'No tiene permisos para descargar este archivo');
+            return back();
+        } catch(Exception $e) {
+            Alert::error('Error', 'ha ocurrido un error');
+            return back();
+        } 
     }
 }
